99dcf1259200a293535d10ad28063e8130fc512a
Robin  Linacre <robinlinacre@hotmail.com>
2015-06-28 18:18:55 +0100

    santitised config

---
+++ website/config.py
@@ -0,0 +1,40 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+if os.path.exists('.env'):
+    # root.info('Importing environment from .env...')
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'd987j'
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join((basedir), 'trade_data_new.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('CONNECTION_STRING_SQLALCHEMY_WINDOWS')
+
+    print SQLALCHEMY_DATABASE_URI
+
+
+
+
+    print SQLALCHEMY_DATABASE_URI
+
+class AzureConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+
+
+
+
+config = {
+    "development": DevelopmentConfig,
+    "azure": AzureConfig,
+    "default": Config
+}
